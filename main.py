6aad8258a4bb4929a4f9153b3f253130746543de
Sam Kumar <samkumarr24@gmail.com>
2026-03-10 18:33:49 +0530

    pytest pipline test case tested

--- main.py
+++ main.py
@@ -8,7 +8,21 @@ from src.recommendation_system.logging import logger
 
 
 # ── Stage 1 — Data Ingestion ──────────────────────────
-
+Stage_name = "Stage 1 : Data Ingestion"
+try:
+    logger.info("")
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  STARTED  <<<<<<")
+    logger.info("=" * 80)
+    data_ingestion = Data_injestion_pipline()
+    data_ingestion.main()
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  COMPLETED  <<<<<<")
+    logger.info("=" * 80)
+    logger.info("")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
 # ── Stage 2 — Data Validation ─────────────────────────
@@ -20,6 +34,78 @@ try:
     logger.info("=" * 80)
     data_validation = Data_vaidation_pipline()
     data_validation.main()
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  COMPLETED  <<<<<<")
+    logger.info("=" * 80)
+    logger.info("")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# ── Stage 3 — Data Transformation ────────────────────
+Stage_name = "Stage 3 : Data Transformation"
+try:
+    logger.info("")
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  STARTED  <<<<<<")
+    logger.info("=" * 80)
+    data_transformation = Data_transformation_pipline()
+    data_transformation.main()
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  COMPLETED  <<<<<<")
+    logger.info("=" * 80)
+    logger.info("")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# ── Stage 4 — Feature Engineering ────────────────────
+Stage_name = "Stage 4 : Feature Engineering"
+try:
+    logger.info("")
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  STARTED  <<<<<<")
+    logger.info("=" * 80)
+    feature_engineering = feature_enginerring_pipline()
+    feature_engineering.main()
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  COMPLETED  <<<<<<")
+    logger.info("=" * 80)
+    logger.info("")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# ── Stage 5 — Model Training ──────────────────────────
+Stage_name = "Stage 5 : Model Training"
+try:
+    logger.info("")
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  STARTED  <<<<<<")
+    logger.info("=" * 80)
+    model_training = model_training_pipline()
+    model_training.main()
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  COMPLETED  <<<<<<")
+    logger.info("=" * 80)
+    logger.info("")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# ── Stage 6 — Model Evaluation ────────────────────────
+Stage_name = "Stage 6 : Model Evaluation"
+try:
+    logger.info("")
+    logger.info("=" * 80)
+    logger.info(f"          >>>>>>  {Stage_name}  STARTED  <<<<<<")
+    logger.info("=" * 80)
+    model_eval = model_eval_pipline()
+    model_eval.main()
     logger.info("=" * 80)
     logger.info(f"          >>>>>>  {Stage_name}  COMPLETED  <<<<<<")
     logger.info("=" * 80)
